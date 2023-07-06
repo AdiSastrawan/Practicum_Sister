23c5e73f63c29393ce1df1a948b55160f7a50541
AdiSastrawan <adisastrawan110703@gmail.com>
2023-07-06 15:40:34 +0800

    second commit

--- web/index.php
+++ web/index.php
@@ -1,1 +1,15 @@
-<?php echo "My name is Adi Sastrawan";
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Docker Practicum</title>
+</head>
+
+<body>
+    <h1>Docker Practicum</h1>
+    <p>Name : Putu Adi Sastrawan.</p>
+    <p>Class : IKI </p>
+    <p>NIM : 2115101011.</p>
+</body>
+
+</html>
